3619510005d6faac46ade4f9b45de7eb52909ab3
sonastea <sonastea@gmail.com>
2021-11-29 21:13:00 -0800

    Enable CORS in NEST app and added config module.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './database/prisma.service';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi  from 'joi';
 
 @Module({
   imports: [],
   controllers: [UserController],
   providers: [PrismaService, UserService],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        PORT: Joi.number().default(5000),
+      }),
+    }),
+  ],
 })
 export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(5000);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: true,
+      credentials: true,
+    },
+  });
+  await app.listen(process.env.PORT);
 }
 bootstrap();
