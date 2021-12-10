72ddd2d76ff0cad92e65b6f65fe837d3f37c807f
sonastea <sonastea@gmail.com>
2021-12-10 03:07:37 -0800

    Redo navbar brand coloring.

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,13 +1,14 @@
-import { Container, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import MobileNav from './MobileNav';
 import NavBarLoggedIn from './NavBarLoggedIn';
 import NavBarLoggedOut from './NavBarLoggedOut';
 
 const NavBar = (props: any) => {
   return (
-    <Navbar id="navbar" variant="light" expand="md" collapseOnSelect sticky="top">
+    <Navbar id="navbar" expand="md" collapseOnSelect sticky="top">
       <Container>
         <Navbar.Brand href="/">KPOPOP</Navbar.Brand>
+        <Nav.Link className="d-none d-md-block" href="memes">Memes</Nav.Link>
         {props.username && <NavBarLoggedIn username={props.username} />}
         <NavBarLoggedOut username={props.username} />
         <MobileNav username={props.username} />
