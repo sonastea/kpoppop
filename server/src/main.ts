33c9032df04858f811e03468fdabbab4e3ed3487
sonastea <sonastea@gmail.com>
2022-01-03 05:10:29 -0800

    Add secrets folder for backend certs to gitignore and https to backend.
    Backend server deploys https if running in production mode.

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,17 +3,17 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import * as cookieParser from 'cookie-parser';
 import * as firebase from 'firebase-admin';
+import * as fs from 'fs';
+import * as path from 'path';
+import { Logger } from '@nestjs/common';
 
 dotenv.config();
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: true,
-      credentials: true,
-    },
-  });
+async function whatMode() {
+  Logger.log(`Running in ${process.env.NODE_ENV} mode`);
+}
 
+async function bootstrap() {
   // Firebase Initialization
   const serviceAccount = require('../firebaseCredentials.json');
   firebase.initializeApp({
@@ -21,7 +21,33 @@ async function bootstrap() {
     storageBucket: 'gs://images.kpoppop.com/',
   });
 
-  app.use(cookieParser());
-  await app.listen(process.env.PORT);
+  const httpsOptions = {
+    key: fs.readFileSync(path.resolve('./secrets/private-key.pem')),
+    cert: fs.readFileSync(path.resolve('./secrets/public-certificate.pem')),
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    const app = await NestFactory.create(AppModule, {
+      cors: {
+        origin: true,
+        credentials: true,
+      },
+      httpsOptions,
+    });
+
+    app.use(cookieParser());
+    await app.listen(process.env.PORT, () => whatMode());
+  } else {
+    const app = await NestFactory.create(AppModule, {
+      cors: {
+        origin: true,
+        credentials: true,
+      },
+    });
+
+    app.use(cookieParser());
+    await app.listen(process.env.PORT, () => whatMode());
+
+  }
 }
 bootstrap();
