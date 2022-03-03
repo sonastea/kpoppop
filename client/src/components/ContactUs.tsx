8b125d0f71b96d5e9294ac7ad6e17a9683a2208b
sonastea <sonastea@gmail.com>
2022-03-02 16:48:45 -0800

    Force text to be on only one line.

--- client/src/components/ContactUs.tsx
+++ client/src/components/ContactUs.tsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const ContactUs = () => {
   return (
     <div className="shadow-md md:w-full md:h-screen grid grid-rows-2 md:flex md:items-center">
-      <div className="py-2 text-thrice md:bg-white md:flex-initial md:mx-20">
+      <div className="py-2 text-thrice md:bg-white md:flex-shrink-0 md:mx-20">
         <h2 className="text-2xl text-center md:text-once-400 md:text-7xl">Contact us</h2>
       </div>
       <div className="items-center w-full h-full md:text-gray-800 md:text-2xl font-semi-bold md:font-semibold md:flex md:bg-gradient-to-r md:from-once-400 md:to-ponce-400">
