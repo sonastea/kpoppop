1098c51d6f6be276183c90551e8b0a922ca9b93b
sonastea <sonastea@gmail.com>
2022-04-30 23:10:14 -0700

    Change eslint no-unused-vars to a warning from error.

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-unused-vars': [
-      'error',
+      'warn',
       { ignoreRestSiblings: true, argsIgnorePattern: '^_' },
     ],
   },
