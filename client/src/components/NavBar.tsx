d7fd6d82f4251aa09a57aad068e0a5f047da1033
sonastea <sonastea@gmail.com>
2023-03-15 04:15:04 -0700

    fix: mobilenav hidden from nav overflow hidden

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -41,9 +41,9 @@ const NavBar = () => {
   }, [location.pathname, navItems]);
 
   return (
-    <nav className="relative w-full shadow overflow-hidden">
+    <nav className="relative w-full shadow">
       <div className="mx-auto md:max-w-screen-2xl">
-        <div className="flex justify-between flex-wrap">
+        <div className="flex justify-between flex-wrap overflow-hidden">
           <div className="flex flex-shrink-0 space-x-2">
             <a href="/" className="flex items-center m-2">
               <img
