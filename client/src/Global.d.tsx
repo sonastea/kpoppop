2d1809c1e37f9d35a1d884581b7c4dd494ed8da3
sonastea <sonastea@gmail.com>
2023-03-16 04:39:51 -0700

    feat: pass config to dayjs for time thresholds

--- client/src/Global.d.tsx
+++ client/src/Global.d.tsx
@@ -1,7 +1,24 @@
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import updateLocale from 'dayjs/plugin/updateLocale';
-dayjs.extend(relativeTime);
+
+const config = {
+  thresholds: [
+    { l: 's', r: 1 },
+    { l: 'm', r: 1 },
+    { l: 'mm', r: 59, d: 'minute' },
+    { l: 'h', r: 1 },
+    { l: 'hh', r: 23, d: 'hour' },
+    { l: 'd', r: 2 },
+    { l: 'dd', r: 29, d: 'day' },
+    { l: 'M', r: 1 },
+    { l: 'MM', r: 11, d: 'month' },
+    { l: 'y', r: 1 },
+    { l: 'yy', d: 'year' },
+  ],
+};
+
+dayjs.extend(relativeTime, config);
 dayjs.extend(updateLocale);
 
 dayjs.updateLocale('en', {
