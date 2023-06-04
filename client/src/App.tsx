88453b4a4d7008accc368a48c45ab7e77ffafba6
sonastea <sonastea@gmail.com>
2023-06-03 19:35:30 -0700

    feat: switch to vite from craco/cra
    - Update dependencies for client, removed tfjs
    - Update dependencies for server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,7 @@ import Messages from 'components/user/Messages';
 import RegisterRedirect from 'components/user/RegisterRedirect';
 import VerifyEmail from 'components/user/VerifyEmail';
 import { AuthProvider } from 'contexts/AuthContext';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Home from './components/Home';
@@ -16,39 +16,48 @@ import Login from './components/user/Login';
 import Profile from './components/user/Profile';
 import Register from './components/user/Register';
 
-const App = () => {
+const Layout = () => {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <ToastContainer
-          autoClose={1000}
-          bodyClassName="toastifyBody"
-          closeOnClick
-          draggable
-          hideProgressBar={true}
-          position="top-right"
-          limit={3}
-        />
-        <NavBar />
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="memes" element={<MemePage />} />
-          <Route path="meme/:memeid/" element={<Post />} />
-          <Route path="meme/:memeid/:title" element={<Post />} />
-          <Route path="contact" element={<ContactUs />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="register/redirect" element={<RegisterRedirect />} />
-          <Route path="email/verify" element={<VerifyEmail />} />
-          <Route path="user/:username" element={<Profile />} />
-          <Route path="messages" element={<Messages />} />
-          <Route path="profile/settings" element={<EditProfile />} />
-          <Route path="employees-only-do-not-enter" element={<AdminPanel />} />
-        </Routes>
-      </BrowserRouter>
+      <ToastContainer
+        autoClose={1000}
+        bodyClassName="toastifyBody"
+        closeOnClick
+        draggable
+        hideProgressBar={true}
+        position="top-right"
+        limit={3}
+      />
+      <NavBar />
+      <Outlet />
     </AuthProvider>
   );
+};
+
+const router = createBrowserRouter([
+  {
+    path: '*',
+    children: [
+      { index: true, Component: Home },
+      { path: 'memes', Component: MemePage },
+      { path: 'meme/:memeid/', Component: Post },
+      { path: 'meme/:memeid/:title', Component: Post },
+      { path: 'contact', Component: ContactUs },
+      { path: 'login', Component: Login },
+      { path: 'register', Component: Register },
+      { path: 'register/redirect', Component: RegisterRedirect },
+      { path: 'email/verify', Component: VerifyEmail },
+      { path: 'user/:username', Component: Profile },
+      { path: 'messages', Component: Messages },
+      { path: 'profile/settings', Component: EditProfile },
+      { path: 'employees-only-do-not-enter', Component: AdminPanel },
+    ],
+    Component: Layout,
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
 };
 
 export default App;
---
+++ client/vite.config.js
@@ -0,0 +1,46 @@
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import react from '@vitejs/plugin-react';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig(() => {
+  return {
+    build: {
+      outDir: 'build',
+      rollupOptions: {
+        plugins: [nodePolyfills()],
+      },
+    },
+    define: {
+      'process.env': {},
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        plugins: [
+          NodeGlobalsPolyfillPlugin({
+            process: true,
+            buffer: true,
+          }),
+          NodeModulesPolyfillPlugin(),
+        ],
+      },
+    },
+    plugins: [basicSsl(), react(), tsconfigPaths()],
+    resolve: {
+      alias: {
+        events: 'rollup-plugin-node-polyfills/polyfills/events',
+        path: 'rollup-plugin-node-polyfills/polyfills/path',
+        stream: 'rollup-plugin-node-polyfills/polyfills/stream',
+        zlib: 'rollup-plugin-node-polyfills/polyfills/zlib',
+      },
+    },
+    server: {
+      open: true,
+      https: true,
+      port: 3000,
+    },
+  };
+});
