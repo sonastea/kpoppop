b3602f1b5e0f21376f06161f33ea832135341a2e
sonastea <sonastea@gmail.com>
2024-03-06 21:32:53 -0800

    feat(client): explicitly load nsfwjs model
    - Update nsfwjs to 4.1.0

--- client/src/components/meme/IdentifyImage.tsx
+++ client/src/components/meme/IdentifyImage.tsx
@@ -1,11 +1,15 @@
 import { PredictionType } from './UploadMeme';
 
 export const identifyImage = async (image: any): Promise<PredictionType> => {
+  // eslint-disable-next-line prefer-const
   let img = new window.Image();
+
   const objectUrl = URL.createObjectURL(image);
   fetch(objectUrl).then((blob) => (img.src = blob.url));
+
   const nsfwjs = await import('nsfwjs');
-  const model = await nsfwjs.load();
+  const model = await nsfwjs.load('/models/inception_v3/model.json', { size: 299 });
   const predictions = await model.classify(img);
+
   return predictions[0];
 };
