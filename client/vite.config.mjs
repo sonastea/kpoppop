d2e718608531d2825f370736e263da4b13eddeb6
sonastea <sonastea@gmail.com>
2024-03-30 16:30:37 -0700

    build(client): try removing polyfills and such

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -1,7 +1,4 @@
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 import react from '@vitejs/plugin-react';
-import nodePolyfills from 'rollup-plugin-polyfill-node';
 import Unfonts from 'unplugin-fonts/vite';
 import { defineConfig } from 'vite';
 import mkcert from 'vite-plugin-mkcert';
@@ -35,23 +32,11 @@ export default defineConfig(() => {
             }
           },
         },
-        plugins: [nodePolyfills()],
       },
       sourcemap: true,
     },
     define: {
       'process.env': {},
-    },
-    optimizeDeps: {
-      esbuildOptions: {
-        plugins: [
-          NodeGlobalsPolyfillPlugin({
-            process: true,
-            buffer: true,
-          }),
-          NodeModulesPolyfillPlugin(),
-        ],
-      },
     },
     plugins: [
       mkcert(),
@@ -63,14 +48,6 @@ export default defineConfig(() => {
         },
       }),
     ],
-    resolve: {
-      alias: {
-        events: 'rollup-plugin-node-polyfills/polyfills/events',
-        path: 'rollup-plugin-node-polyfills/polyfills/path',
-        stream: 'rollup-plugin-node-polyfills/polyfills/stream',
-        zlib: 'rollup-plugin-node-polyfills/polyfills/zlib',
-      },
-    },
     server: {
       https: true,
       open: true,
