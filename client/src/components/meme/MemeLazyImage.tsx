39a76370f7a815ea99e295c829b55f73d9a6eca2
sonastea <sonastea@gmail.com>
2024-04-30 00:35:32 -0700

    feat: media lightbox for images and videos (#55)
    * feat: set post media to fit as cover and margin to match
    
    * feat: resize image and adjust screen width breakpoint
    
    * feat: media lightbox
    
    mend
    
    * feat: media lightbox video support

--- client/src/components/meme/MemeLazyImage.tsx
+++ client/src/components/meme/MemeLazyImage.tsx
@@ -1,3 +1,5 @@
+import MediaLightbox from 'components/MediaLightbox';
+import { useState } from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 type MemeLazyImageProps = {
@@ -12,46 +14,70 @@ const PlaceHolder = () => {
   return (
     <div
       className="mt-2 flex h-96 max-h-64 w-full items-center justify-center rounded bg-gray-200
-        object-scale-down md:max-h-96 md:object-contain"
+        object-cover sm:max-h-96 sm:object-contain"
     />
   );
 };
 
 const MemeLazyImage: React.FC<MemeLazyImageProps> = ({ id, src, title, alt, visibleByDefault }) => {
+  const [openLightbox, setOpenLightbox] = useState(false);
   const isVideo = src.split('.')[3] === 'mp4';
+
+  const src336 = `${src}?tr=w-336`;
+  const src640 = `${src}?tr=w-640`;
+
+  const toggleMediaLightBox = () => {
+    setOpenLightbox((state) => !state);
+  };
 
   if (isVideo) {
     return (
-      <video
-        key={title}
-        className="mx-auto aspect-square w-full object-cover md:aspect-auto md:max-h-96"
-        autoPlay
-        controls
-        muted
-        loop
-        src={src}
-      >
-        <source src={src} type="video/mp4" />
-      </video>
+      <button className="mx-4 w-full" onClick={() => toggleMediaLightBox()} aria-label={alt}>
+        <MediaLightbox
+          open={openLightbox}
+          setOpen={toggleMediaLightBox}
+          src={src}
+          alt={alt}
+          isVideo={true}
+        />
+        <video
+          key={title}
+          className="aspect-square w-full rounded-md object-cover sm:aspect-auto sm:max-h-96"
+          autoPlay
+          controls
+          muted
+          loop
+          src={src}
+        >
+          <source src={src} type="video/mp4" />
+        </video>
+      </button>
     );
   }
 
   return (
-    <a className="contents" href={`/meme/${id}/${title}`} aria-label={title}>
-      <picture className="inline-block">
-        <source media="(max-width: 639px)" srcSet={`${src}?tr=w-336`} />
-        <source media="(min-width: 640px)" srcSet={`${src}?tr=w-672`} />
+    <button className="mx-4 w-full" onClick={() => toggleMediaLightBox()} aria-label={alt}>
+      <MediaLightbox
+        open={openLightbox}
+        setOpen={toggleMediaLightBox}
+        src={src}
+        alt={alt}
+        isVideo={false}
+      />
+      <picture className="grid w-full" data-image-id={id}>
+        <source media="(max-width: 639px)" srcSet={src336} />
+        <source media="(min-width: 640px)" srcSet={src640} />
         <LazyLoadImage
           effect="blur"
-          className="mt-2 h-64 max-h-64 w-[336px] object-scale-down md:h-96 md:max-h-96 md:w-[672px]
-            md:object-contain"
-          src={src}
+          className="mt-2 flex h-64 max-h-64 w-full rounded-md object-cover sm:h-96 sm:max-h-96
+            sm:w-[672px]"
+          src={src640}
           alt={alt}
           visibleByDefault={visibleByDefault}
           placeholder={<PlaceHolder />}
         />
       </picture>
-    </a>
+    </button>
   );
 };
 
