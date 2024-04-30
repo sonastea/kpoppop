7a412edb7a5ef0cbdb37b8e1fd0a073977acde61
sonastea <sonastea@gmail.com>
2024-04-29 21:51:39 -0700

    feat: resize image and adjust screen width breakpoint

--- client/src/components/meme/MemeLazyImage.tsx
+++ client/src/components/meme/MemeLazyImage.tsx
@@ -12,7 +12,7 @@ const PlaceHolder = () => {
   return (
     <div
       className="mt-2 flex h-96 max-h-64 w-full items-center justify-center rounded bg-gray-200
-        object-cover md:max-h-96 md:object-contain"
+        object-cover sm:max-h-96 sm:object-contain"
     />
   );
 };
@@ -20,11 +20,14 @@ const PlaceHolder = () => {
 const MemeLazyImage: React.FC<MemeLazyImageProps> = ({ id, src, title, alt, visibleByDefault }) => {
   const isVideo = src.split('.')[3] === 'mp4';
 
+  const src336 = `${src}?tr=w-336`;
+  const src640 = `${src}?tr=w-640`;
+
   if (isVideo) {
     return (
       <video
         key={title}
-        className="mx-4 aspect-square w-full rounded-md object-cover md:aspect-auto md:max-h-96"
+        className="mx-4 aspect-square w-full rounded-md object-cover sm:aspect-auto sm:max-h-96"
         autoPlay
         controls
         muted
@@ -37,21 +40,19 @@ const MemeLazyImage: React.FC<MemeLazyImageProps> = ({ id, src, title, alt, visi
   }
 
   return (
-    <a className="contents" href={`/meme/${id}/${title}`} aria-label={title}>
-      <picture className="mx-4 grid w-full">
-        <source media="(max-width: 639px)" srcSet={`${src}?tr=w-336`} />
-        <source media="(min-width: 640px)" srcSet={`${src}?tr=w-640`} />
-        <LazyLoadImage
-          effect="blur"
-          className="mt-2 flex h-64 max-h-64 w-full rounded-md object-cover md:h-96 md:max-h-96
-            md:w-[672px]"
-          src={src}
-          alt={alt}
-          visibleByDefault={visibleByDefault}
-          placeholder={<PlaceHolder />}
-        />
-      </picture>
-    </a>
+    <picture className="mx-4 grid w-full" data-image-id={id}>
+      <source media="(max-width: 639px)" srcSet={src336} />
+      <source media="(min-width: 640px)" srcSet={src640} />
+      <LazyLoadImage
+        effect="blur"
+        className="mt-2 flex h-64 max-h-64 w-full rounded-md object-cover sm:h-96 sm:max-h-96
+          sm:w-[672px]"
+        src={src640}
+        alt={alt}
+        visibleByDefault={visibleByDefault}
+        placeholder={<PlaceHolder />}
+      />
+    </picture>
   );
 };
 
