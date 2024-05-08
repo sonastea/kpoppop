b18bf38ff5edee8abd4aeff3a47900b0ae11e799
sonastea <sonastea@gmail.com>
2024-05-07 20:42:02 -0700

    feat: migrate user menu to floating-ui

--- client/src/components/user/UserMenu.tsx
+++ client/src/components/user/UserMenu.tsx
@@ -1,55 +1,79 @@
+import {
+  autoUpdate,
+  flip,
+  offset,
+  shift,
+  useClick,
+  useDismiss,
+  useFloating,
+  useInteractions,
+  useTransitionStyles,
+} from '@floating-ui/react';
 import { faEllipsis } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Popover, PopoverButton, PopoverPanel, Transition } from '@headlessui/react';
 import { useAuth } from 'contexts/AuthContext';
 import { useEffect, useState } from 'react';
-import { usePopper } from 'react-popper';
-import useUserMenuButtons from './hooks/useUserMenuButtons';
 import { UserTooltipProps as UserMenuProps } from './UserTooltip';
+import useUserMenuButtons from './hooks/useUserMenuButtons';
 
 const UserMenu = ({ comment }: UserMenuProps) => {
-  const [referenceElement, setReferenceElement] = useState<HTMLElement | null>(null);
-  const [popperElement, setPopperElement] = useState<HTMLElement | null>(null);
   const { DefaultMenuButtons, ModerationMenuButtons } = useUserMenuButtons({ comment });
-  const { styles, attributes } = usePopper(referenceElement, popperElement);
   const [isAuthorized, setAuthorized] = useState<boolean>(false);
+  const [open, setOpen] = useState(false);
   const { user } = useAuth();
+
+  const { context, refs, floatingStyles } = useFloating({
+    open: open,
+    onOpenChange: setOpen,
+    whileElementsMounted: autoUpdate,
+    middleware: [
+      offset(({ rects }) => {
+        return -rects.reference.height / 2 - rects.floating.height / 2;
+      }),
+      flip(),
+      shift(),
+    ],
+  });
+
+  const { isMounted, styles: transitionStyles } = useTransitionStyles(context, {
+    initial: {
+      opacity: 0,
+    },
+    close: {
+      opacity: 0,
+    },
+    open: {
+      opacity: 1,
+    },
+    duration: {
+      open: 75,
+      close: 150,
+    },
+  });
+
+  const dismiss = useDismiss(context);
+  const click = useClick(context);
+  const { getReferenceProps, getFloatingProps } = useInteractions([click, dismiss]);
 
   useEffect(() => {
     if (user?.role === 'MODERATOR' || user?.role === 'ADMIN') setAuthorized(true);
   }, [user?.role]);
 
-  styles.popper = {
-    ...styles.popper,
-    position: 'absolute',
-    inset: '-30px auto auto -20px',
-  };
-
   return (
-    <>
-      <Popover>
-        <PopoverButton
-          className="rounded-full px-1 hover:bg-slate-200 focus-visible:bg-slate-200
-            focus-visible:outline-offset-2"
-          ref={setReferenceElement}
-          aria-label="User menu"
-        >
-          <FontAwesomeIcon icon={faEllipsis} />
-        </PopoverButton>
-        <Transition
-          enter="transition-opacity duration-150 ease-in-out"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="transition-opacity duration-100 ease-in-out"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <PopoverPanel
-            className="absolute z-100"
-            ref={setPopperElement}
-            style={styles.popper}
-            {...attributes.popper}
-          >
+    <div>
+      <button
+        className="rounded-full px-1 hover:bg-slate-200 focus-visible:bg-slate-200
+          focus-visible:outline-offset-2"
+        ref={refs.setReference}
+        aria-label="User menu"
+        role="button"
+        {...getReferenceProps()}
+      >
+        <FontAwesomeIcon icon={faEllipsis} />
+      </button>
+      {isMounted && (
+        <div style={transitionStyles}>
+          <div className="" ref={refs.setFloating} style={floatingStyles} {...getFloatingProps()}>
             <div
               className="overflow-hidden rounded-md border border-gray-200 bg-white text-sm
                 shadow-sm"
@@ -57,10 +81,10 @@ const UserMenu = ({ comment }: UserMenuProps) => {
               {DefaultMenuButtons}
               {isAuthorized && user?.id !== comment.user.id && ModerationMenuButtons}
             </div>
-          </PopoverPanel>
-        </Transition>
-      </Popover>
-    </>
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 export default UserMenu;
