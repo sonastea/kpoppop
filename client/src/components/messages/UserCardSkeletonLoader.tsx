8dbed9e40bf85fecae8a7ceab82c63a3921a4c07
sonastea <sonastea@gmail.com>
2024-09-03 01:29:49 -0700

    style: messages skeleton loader & add `messages` header
    - Made the messages skeleton loader to look more like the actual
    component by adding missing header and styling adjustments.

--- client/src/components/messages/UserCardSkeletonLoader.tsx
+++ client/src/components/messages/UserCardSkeletonLoader.tsx
@@ -4,9 +4,9 @@ const MaxUserCards = new Array(Math.floor(Math.random() * 9) + 1).fill(null);
 
 const UserCardSkeletonLoader = () => {
   return (
-    <div className="flex md:mx-2 min-h-0">
-      <div className="md:flex flex-col w-full md:w-2/5">
-        <ul className="basis-full w-full min-h-0 overflow-auto no-scrollbar p-4">
+    <div className="flex min-h-0 md:mx-2">
+      <div className="w-full flex-col md:flex md:w-2/5">
+        <ul className="no-scrollbar min-h-0 w-full basis-full overflow-auto">
           {MaxUserCards.map((_, index) => {
             return <UserCardSkeleton key={index} />;
           })}
