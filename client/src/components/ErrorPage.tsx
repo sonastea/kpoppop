0949823abe50fe961d3fe98f12f558ee07fea967
sonastea <sonastea@gmail.com>
2024-09-09 05:14:42 -0700

    style: error page message padding & nav aware h-screen

--- client/src/components/ErrorPage.tsx
+++ client/src/components/ErrorPage.tsx
@@ -2,10 +2,13 @@ import { Link } from 'react-router-dom';
 
 const ErrorPage: React.FC = () => {
   return (
-    <div className="flex h-screen w-full items-center justify-center bg-gray-200/60">
+    <div
+      className="flex w-full items-center justify-center bg-gray-200/60 h-screen-mobile
+        sm:h-screen-larger"
+    >
       <span
         className="absolute mx-auto box-content flex w-fit select-none border bg-gradient-to-r
-          from-once-600 to-ponce-600 bg-clip-text text-center text-6xl font-extrabold
+          from-once-600 to-ponce-600 bg-clip-text py-2 text-center text-6xl font-extrabold
           text-transparent blur-xl"
       >
         Back to kpoppop
@@ -13,7 +16,7 @@ const ErrorPage: React.FC = () => {
       <Link
         to="/"
         className="relative top-0 flex h-auto w-fit select-auto items-center justify-center
-          bg-gradient-to-r from-once-600 to-ponce-600 bg-clip-text text-center text-6xl
+          bg-gradient-to-r from-once-600 to-ponce-600 bg-clip-text py-2 text-center text-6xl
           font-extrabold text-transparent hover:text-thrice"
       >
         Back to kpoppop
